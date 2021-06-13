6bf13a6c4cea257a3c7afc5f1bbfa6a1a2729ef7
BarisKocaman55 <54405338+BarisKocaman55@users.noreply.github.com>
2021-06-13 13:59:21 +0300

    Added Entities Layer

---
+++ SerenityTestSolution/Entities/Concrete/Admin.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Admin : IEntity
+    {
+        [Key, Column(Order = 1)]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$")]
+        public string Password { get; set; }
+        
+    }
+}
---
+++ SerenityTestSolution/Entities/Concrete/User.cs
@@ -0,0 +1,42 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class User : IEntity
+    {
+        [Key, Column(Order = 1)]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string FirtstName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(0(\d{3})-(\d{3})-(\d{2})-(\d{2}))$")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Location { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$")]
+        public string Password { get; set; }
+        
+    }
+}
